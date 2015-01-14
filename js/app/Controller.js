8447d83b765b3cbc230062f68704c68778bb2e34
cburgos <cburgos294@gmail.com>
2015-01-14 18:49:10 -0500

    Fixed App structure

---
+++ js/app/Controller.js
@@ -0,0 +1,20 @@
+﻿define([
+    "dojo/_base/declare",
+    "esri/map",
+    "application/widgets/bootstrapmap"
+],
+function (declare, Map, BootstrapMap) {
+    return declare(null, {
+        startup: function () {
+            console.log("app startup...");
+        },
+        initMap: function () {
+            var map = BootstrapMap.create("mapDiv", {
+                basemap: "national-geographic",
+                center: [-122.45, 37.77],
+                zoom: 12
+            });
+        }
+
+    });// return
+}); //define
