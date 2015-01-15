cce65ad06299e015905cd55b6a1f5cf1cc392177
cburgos <cburgos294@gmail.com>
2015-01-15 01:17:47 -0500

    config
    added image url field

--- js/app/config.js
+++ js/app/config.js
@@ -6,5 +6,6 @@
     ],
     pageTitle: "",
     webmapId: "a44443cefef84817a83fb2b8b208cd77",
-    shortlistDisplayField: "PROJ_NO"
+    shortlistDisplayField: "PROJ_NO",
+    imageField :"IMAGE_URL"
 });
