61bc64ce74735e4fd3c90ae079732c325b315b50
cburgos <cburgos294@gmail.com>
2015-01-21 07:59:16 -0500

    Layer Control
    Added layer control

---
+++ js/app/widgets/LayerControl.js
@@ -0,0 +1,26 @@
+﻿define([
+    "application/config",
+    "dojo/_base/declare",
+    "dojo/_base/array",
+    "dojo/_base/lang",
+    "dojo/dom-construct",
+    "dojo/dom-class",
+    "dojo/on",
+    "dijit/_WidgetBase",
+    "dijit/_TemplatedMixin",
+    "dijit/_WidgetsInTemplateMixin",
+    "dojo/text!application/widgets/templates/LayerControl.html"
+],
+function (config, declare, array, lang, domConstruct, domClass, on, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, template) {
+    return declare([_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
+        templateString: template,
+        map: null,
+        constructor: function () {
+            this.inherited(arguments);
+        },
+        startup: function () {
+
+        }
+
+    });// return
+}); //define
