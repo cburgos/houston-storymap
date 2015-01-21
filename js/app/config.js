dba6d593c5744a93a1ecf766b843e6d904952879
cburgos <cburgos294@gmail.com>
2015-01-21 08:41:40 -0500

    layer control

--- js/app/config.js
+++ js/app/config.js
@@ -11,14 +11,40 @@
         {
             label: "Streets & Drainage",
             webmapId: "a44443cefef84817a83fb2b8b208cd77",
-            referenceLayers: [],
+            referenceLayers: [
+            {
+                url: "http://mycity.houstontx.gov/ArcGIS/rest/services/dd/COHData_dd/MapServer/7",
+                label: "Super Neighborhoods"
+            },
+            {
+                url: "http://mycity.houstontx.gov/ArcGIS/rest/services/dd/COHData_dd/MapServer/2",
+                label: "Zip Codes"
+            },
+            {
+                url: "http://mycity.houstontx.gov/ArcGIS/rest/services/dd/COHData_dd/MapServer/3",
+                label: "City Council Districts"
+            }
+            ],
             checked : true
 
         },
         {
             label: "Water & Wastewater",
             webmapId: "42483ddf05bc43f28aee27b449da0a9b",
-            referenceLayers: [],
+            referenceLayers: [
+            {
+                url: "http://mycity.houstontx.gov/ArcGIS/rest/services/dd/COHData_dd/MapServer/7",
+                label: "Super Neighborhoods"
+            },
+            {
+                url: "http://mycity.houstontx.gov/ArcGIS/rest/services/dd/COHData_dd/MapServer/2",
+                label: "Zip Codes"
+            },
+            {
+                url: "http://mycity.houstontx.gov/ArcGIS/rest/services/dd/COHData_dd/MapServer/3",
+                label: "City Council Districts"
+            }
+            ],
             checked : false
         }
     ]
--- js/app/widgets/LayerControl.js
+++ js/app/widgets/LayerControl.js
@@ -9,18 +9,56 @@
     "dijit/_WidgetBase",
     "dijit/_TemplatedMixin",
     "dijit/_WidgetsInTemplateMixin",
-    "dojo/text!application/widgets/templates/LayerControl.html"
+    "dojo/text!application/widgets/templates/LayerControl.html",
+    "esri/layers/FeatureLayer"
 ],
-function (config, declare, array, lang, domConstruct, domClass, on, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, template) {
+function (config, declare, array, lang, domConstruct, domClass, on, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, template, FeatureLayer) {
     return declare([_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
         templateString: template,
         map: null,
+        webmap: null,
+        layers: null,
         constructor: function () {
             this.inherited(arguments);
+            this.layers = [];
         },
         startup: function () {
+            this._createInputs();
+        },
+        _createInputs: function () {
+            //foreach reference layer create layer, add to map, and add input
+            domConstruct.empty(this.layerListContainer);
 
+            array.forEach(this.webmap.referenceLayers, lang.hitch(this, function (refLayer) {
+                var li = domConstruct.create("li", {
+                    "class": "list-group-item"
+                }, this.layerListContainer, "last");
+                var label = domConstruct.create("label", {
+                    "class": "btn btn-primary",
+                    innerHTML: "&nbsp;"+ refLayer.label
+                }, li, "last");
+                var input = domConstruct.create("input", {
+                    "type": "checkbox",
+                    "autocomplete": "off"
+                }, label, "first");
+
+                //Create layer and add to map
+                var layer = new FeatureLayer(refLayer.url, {
+                    visible:false
+                });
+                this.map.addLayer(layer);
+                this.layers.push(layer);
+                // Handle click
+                on(li, "click", function () {
+                    layer.setVisibility(!layer.visible);
+                });
+            }));
+        },
+        destroy: function () {
+            array.forEach(this.layers, lang.hitch(this, function (layer) {
+                this.map.removeLayer(layer);
+            }));
+            this.inherited(arguments);
         }
-
     });// return
 }); //define
