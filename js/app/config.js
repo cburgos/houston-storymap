aa87ed21731d38d8c88cef48517a522ce611d937
cburgos <cburgos294@gmail.com>
2015-01-21 02:16:26 -0500

    Config
    updated config to support multiple webmaps

--- js/app/config.js
+++ js/app/config.js
@@ -5,7 +5,21 @@
 
     ],
     pageTitle: "",
-    webmapId: "a44443cefef84817a83fb2b8b208cd77",
-    shortlistDisplayField: "PROJ_NO",
-    imageField :"IMAGE_URL"
+    shortlistDisplayField: "PRO_SCOPE",
+    imageField: "PROJ_IMAGE",
+    webmaps : [
+        {
+            label: "Streets & Drainage",
+            webmapId: "a44443cefef84817a83fb2b8b208cd77",
+            referenceLayers: [],
+            checked : true
+
+        },
+        {
+            label: "Water & Wastewater",
+            webmapId: "42483ddf05bc43f28aee27b449da0a9b",
+            referenceLayers: [],
+            checked : false
+        }
+    ]
 });
